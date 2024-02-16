9e4151a84f7b672c59bb01a90635a36da8c81dd3
Md Danish <mohddanish@seera.sa>
2024-02-17 00:20:01 +0400

    changs

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -22,7 +22,7 @@ const Products = () => {
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true);
-      const response = await fetch("https://fakestoreapi.com/products");
+      const response = await fetch("http://20.235.242.6:8080/products");
       if (componentMounted) {
         setData(await response.clone().json());
         setFilter(await response.json());
